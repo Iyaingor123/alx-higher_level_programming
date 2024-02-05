2754e4442041ac88b1ff6a5ecd5827dbdd99dd6a
iyaingor123 <pri_dekk2001@yahoo.com>
2024-02-05 14:38:32 -0800

    safeprint

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -10,4 +9,3 @@ def safe_print_integer_err(value):
         return False
     else:
         return True
-
