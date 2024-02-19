d519508bbcba8cfb3dd95363827b2f46faa4d6e3
iyaingor123 <pri_dekk2001@yahoo.com>
2024-02-19 13:47:04 -0800

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-"""Defines aan object feature lookup function."""
+"""Defines aan object attribute lookup function."""
 
 
 def lookup(obj):
-    """Return a list of an objects available feature."""
-    return (dir[obj]);
+    """Return a list of an object's available attribute."""
+    return (dir(obj));
